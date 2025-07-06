d13d417efe85b288f3cb47d30c8299ef52b9940c
CoolKid <161092074+CoolKid1243@users.noreply.github.com>
2025-07-06 15:15:33 -0400

    Updated default wave settings

--- src/settings.h
+++ src/settings.h
@@ -33,17 +33,17 @@ public:
 
     // Wave settings
     // Wave A
-    inline static glm::vec2 waveDirectionA = glm::normalize(glm::vec2(1.0f, 1.0f));
-    inline static float waveSteepnessA = 0.25f;
-    inline static float waveLengthA = 6.0f;
+    inline static glm::vec2 waveDirectionA = glm::normalize(glm::vec2(-0.739f, 0.674f));
+    inline static float waveSteepnessA = 0.446f;
+    inline static float waveLengthA = 5.281f;
     // Wave B
-    inline static glm::vec2 waveDirectionB = glm::normalize(glm::vec2(1.0f, 0.6f));
-    inline static float waveSteepnessB = 0.25f;
-    inline static float waveLengthB = 3.1f;
+    inline static glm::vec2 waveDirectionB = glm::normalize(glm::vec2(-0.804f, 0.595f));
+    inline static float waveSteepnessB = 0.281f;
+    inline static float waveLengthB = 1.582f;
     // Wave C
-    inline static glm::vec2 waveDirectionC = glm::normalize(glm::vec2(1.0f, 1.3f));
-    inline static float waveSteepnessC = 0.25f;
-    inline static float waveLengthC = 1.8f;
+    inline static glm::vec2 waveDirectionC = glm::normalize(glm::vec2(-0.337f, 0.942f));
+    inline static float waveSteepnessC = 0.311f;
+    inline static float waveLengthC = 2.838f;
 
     // Wave shader
     inline static glm::vec3 waterDeepColor = glm::vec3(0.0f, 0.1f, 0.3f);
